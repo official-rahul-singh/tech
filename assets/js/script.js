c249d3d86217095d2d52fc4a108fed6acb5ec154
official-rahul-singh <battersea5050@gmail.com>
2024-04-16 13:08:44 +0530

    first changes

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+
+// menu
+let headerUl = document.querySelector('header nav');
+
+function toggleButtons() {
+  let header = document.querySelector("header");
+  headerUl.classList.toggle("show-ul");
+let cancel_btn= document.querySelector(".cancel-btn");
+
+  if (!headerUl.classList.contains("show-ul")) {
+    document.querySelector('.doc-overlay').remove();
+    enableScroll();
+  } else {
+    let docOverlayDiv = document.createElement('div');
+    header.appendChild(docOverlayDiv);
+    docOverlayDiv.classList.add('doc-overlay');
+    disableScroll();
+    cancel_btn.style.display="block";
+
+    docOverlayDiv.addEventListener('click', function (event) {
+      headerUl.classList.remove("show-ul");
+      docOverlayDiv.remove();
+      enableScroll();
+    });
+  }
+}
+
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+  document.body.style.overflow = 'auto';
+}
+
+
+
+
+// mobile Dropdown  ============ start =====>
+const navDropdowns = document.querySelectorAll(".dropdown");
+navDropdowns.forEach((parentDropdown) => {
+parentDropdown.addEventListener("click", function (e) {
+this.classList.toggle("showMenu");
+});
+
+const subDropdowns = parentDropdown.querySelectorAll(".dropdown ul");
+subDropdowns.forEach((subDropdown) => {
+subDropdown.addEventListener("click", function (event) {
+   event.stopPropagation(); // Prevents the click event from reaching the parent dropdown
+});
+});
+});
+
+// Add a click event listener to the document to close dropdowns when clicking outside
+document.addEventListener("click", (e) => {
+navDropdowns.forEach((dropdown) => {
+if (!dropdown.contains(e.target)) {
+   dropdown.classList.remove("showMenu");
+}
+});
+});
