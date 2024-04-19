908296f3eec3a3157e65e24f2d087751225ddfae
official-rahul-singh <battersea5050@gmail.com>
2024-04-19 12:00:35 +0530

    another commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,3 +168,24 @@ accountSlider([
   { className: 'slider-name-second', slidesPerView: 1, spaceBetween: 30 }
 ]);
 // testimonial slider END========
+
+
+
+// menu search ==== START==>
+// let search_btn = document.querySelector(".search-btn");
+
+let searchIcon = document.querySelector(".search-btn");
+let searchForm = document.querySelector(".search-input");
+let svg1 =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 16 16" fill="none"><path d="M7.333 12.667A5.333 5.333 0 1 0 7.333 2a5.333 5.333 0 0 0 0 10.667ZM14 14l-2.9-2.9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
+let svg2 =
+    '<svg xmlns="http://www.w3.org/2000/svg" height="18" width="18" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
+let isSvg1 = true;
+searchIcon.addEventListener("click", function () {
+    searchIcon.innerHTML = isSvg1 ? svg2 : svg1;
+    isSvg1 = !isSvg1;
+
+    searchForm.classList.toggle("search-bar-show");
+});
+
+// menu search ==== END==>
